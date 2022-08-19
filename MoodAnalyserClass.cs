d60f5e6e6cc015fb1c4e32ba828192ec34aea66a
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-19 17:58:19 +0530

    Program to analyse and respond sad mood

---
+++ MoodAnalyserClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public  class MoodAnalyserClass
+    {
+        public string message;
+
+        public MoodAnalyserClass(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            if (this.message.Contains("SAD"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
