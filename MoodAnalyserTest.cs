7d75b7963b4bf42ebb4fda9dab8bd4eff1a9a205
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-22 08:09:53 +0530

    Program for creating object and checking equal or not

--- MoodAnalyserTest.cs
+++ MoodAnalyserTest.cs
@@ -6,99 +6,153 @@ namespace MoodAnalyserMSTest
     [TestClass]
     public class MoodAnalyserTest
     {
-        [TestMethod]
-        public void GivenSadMoodShouldReturnSad()
-        {
-            string excepted = "SAD";
-            string message = "I am in SAD Mood";
-            MoodAnalyserClass moodAnalyser = new MoodAnalyserClass(message);
+        //[TestMethod]
+        //public void GivenSadMoodShouldReturnSad()
+        //{
+        //    string excepted = "SAD";
+        //    string message = "I am in SAD Mood";
+        //    MoodAnalyserClass moodAnalyser = new MoodAnalyserClass(message);
 
-            string mood = moodAnalyser.AnalyseMood();
+        //    string mood = moodAnalyser.AnalyseMood();
 
-            Assert.AreEqual(excepted, mood);
+        //    Assert.AreEqual(excepted, mood);
 
-        }
+        //}
 
-        [TestMethod]
-        public void GivenAnyMoodShouldReturnHappy()
-        {
-            string excepted = "HAPPY";
-            string message = "I am in Any Mood";
-            MoodAnalyserClass moodAnalyser = new MoodAnalyserClass(message);
+        //[TestMethod]
+        //public void GivenAnyMoodShouldReturnHappy()
+        //{
+        //    string excepted = "HAPPY";
+        //    string message = "I am in Any Mood";
+        //    MoodAnalyserClass moodAnalyser = new MoodAnalyserClass(message);
 
-            string mood = moodAnalyser.AnalyseMood();
+        //    string mood = moodAnalyser.AnalyseMood();
 
-            Assert.AreEqual(excepted, mood);
+        //    Assert.AreEqual(excepted, mood);
 
-        }
+        //}
 
-        [TestMethod]
-        public void GivenHappyMoodShouldReturnHappy()
-        {
-            string excepted = "HAPPY";
-            string message = "I am in HAPPY Mood";
-            MoodAnalyserClass moodAnalyser = new MoodAnalyserClass(message);
+        //[TestMethod]
+        //public void GivenHappyMoodShouldReturnHappy()
+        //{
+        //    string excepted = "HAPPY";
+        //    string message = "I am in HAPPY Mood";
+        //    MoodAnalyserClass moodAnalyser = new MoodAnalyserClass(message);
 
-            string mood = moodAnalyser.AnalyseMood();
+        //    string mood = moodAnalyser.AnalyseMood();
 
-            Assert.AreEqual(excepted, mood);
+        //    Assert.AreEqual(excepted, mood);
 
-        }
+        //}
 
 
-            [TestMethod]
-            [DataRow(null)]
-            public void GivenHappyMood_ShouldReturnHappy(string message)
-            {
-                string excepted = "HAPPY";
+        //    [TestMethod]
+        //    [DataRow(null)]
+        //    public void GivenHappyMood_ShouldReturnHappy(string message)
+        //    {
+        //        string excepted = "HAPPY";
                
-                MoodAnalyserClass moodAnalyser = new MoodAnalyserClass(message);
+        //        MoodAnalyserClass moodAnalyser = new MoodAnalyserClass(message);
 
-                string mood = moodAnalyser.AnalyseMood();
+        //        string mood = moodAnalyser.AnalyseMood();
 
-                Assert.AreEqual(excepted, mood);
+        //        Assert.AreEqual(excepted, mood);
 
-            }
+        //    }
+
+
+        //[TestMethod]
+        //public void Given_Null_Mood_Should_Throw_MoodAnalysisException()
+        //{
+        //    try
+        //    {
+        //        string message = null;
+        //        MoodAnalyser1 moodAnalyser = new MoodAnalyser1(message);
+        //        string mood = moodAnalyser.analyseMood();
+
+        //    }
+
+        //    catch (MoodAnalyserCustomException e)
+        //    {
+        //        Assert.AreEqual("Mood should not be empty", e.Message);
+        //    }
+        //}
+
+
+
+        //[TestMethod]
+        //public void Given_Empty_Mood_Throw_MoodAnalysisException_Indicating_EmptyMood()
+        //{
+        //    try
+        //    {
+        //        string message = "";
+        //        MoodAnalyser1 moodAnalyser = new MoodAnalyser1(message);
+        //        string mood = moodAnalyser.analyseMood();
+
+        //    }
+
+        //    catch(MoodAnalyserCustomException e)
+        //    {
+        //        Assert.AreEqual("Mood should not be empty", e.Message);
+        //    }
+        //}
 
 
         [TestMethod]
-        public void Given_Null_Mood_Should_Throw_MoodAnalysisException()
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
         {
-            try
-            {
-                string message = null;
-                MoodAnalyser1 moodAnalyser = new MoodAnalyser1(message);
-                string mood = moodAnalyser.analyseMood();
+            string message = null;
+            object expected = new MoodAnalyser1(message);
+            object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserAppWithCore.MoodAnalyser", "MoodAnalyser");
+           expected.Equals(obj);
+            
 
-            }
-
-            catch (MoodAnalyserCustomException e)
-            {
-                Assert.AreEqual("Mood should not be empty", e.Message);
-            }
         }
 
 
 
-        [TestMethod]
-        public void Given_Empty_Mood_Throw_MoodAnalysisException_Indicating_EmptyMood()
-        {
-            try
-            {
-                string message = "";
-                MoodAnalyser1 moodAnalyser = new MoodAnalyser1(message);
-                string mood = moodAnalyser.analyseMood();
-
-            }
-
-            catch(MoodAnalyserCustomException e)
-            {
-                Assert.AreEqual("Mood should not be empty", e.Message);
-            }
-        }
 
 
-       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
